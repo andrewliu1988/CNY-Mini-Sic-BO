1a04495fc17ed75b73a84aeed3ee3c4cfdce46e2
Andrew Liu <liu.andrew1988@gmail.com>
2021-02-08 13:02:10 -0500

    create random function for diceOne . test on console of browser

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // console.log('hello world')
 
-let diceOne = document.querySelector('.diceOne')
-let diceTwo = document.querySelector('.diceTwo')
-let diceThree = document.querySelector('.diceThree')
+// let diceOne = document.querySelector('.diceOne')
+// let diceTwo = document.querySelector('.diceTwo')
+// let diceThree = document.querySelector('.diceThree')
 let diceTotal = document.querySelector('.diceTotal')
 const big = document.querySelector('.big')
 const small = document.querySelector('.small')
 let betAmmount = document.querySelector('#betAmmount')
 const betBtn = document.querySelector('.betBtn')
 const threeOfAKind = document.querySelectorAll('.three')
+
+function diceOne(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+console.log(diceOne(1, 7))
