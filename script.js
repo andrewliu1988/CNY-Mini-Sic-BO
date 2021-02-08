ca5147642addb37b1b5cf2850d08c8aa34baa4ce
Andrew Liu <liu.andrew1988@gmail.com>
2021-02-08 13:55:06 -0500

    add event listener for bet button and add dice two and three random function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // let diceOne = document.querySelector('.diceOne')
 // let diceTwo = document.querySelector('.diceTwo')
 // let diceThree = document.querySelector('.diceThree')
-let diceTotal = document.querySelector('.diceTotal')
+// let diceTotal = document.querySelector('.diceTotal')
 const big = document.querySelector('.big')
 const small = document.querySelector('.small')
 let betAmmount = document.querySelector('#betAmmount')
@@ -15,5 +15,25 @@ function diceOne(min, max) {
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min) + min)
 }
-
 console.log(diceOne(1, 7))
+
+function diceTwo(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min)
+}
+console.log(diceTwo(1, 7))
+
+function diceThree(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min)
+}
+console.log(diceThree(1, 7))
+
+const placeBet = function (event) {
+  let bet = event.target
+}
+console.log(placeBet)
+
+betBtn.addEventListener('click', placeBet)
