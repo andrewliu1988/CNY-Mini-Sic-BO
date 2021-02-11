8fdbee924927dbe97d2f2428853387c6e385c4b7
Andrew Liu <liu.andrew1988@gmail.com>
2021-02-10 23:44:49 -0500

    refactor global variable to become eventlistener

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 let diceOne = document.querySelector('#dieOne')
-
 let diceTwo = document.querySelector('#dieTwo')
 let diceThree = document.querySelector('#dieThree')
 let diceResult = document.querySelector('#total')
-const big = document.querySelector('.big')
-const small = document.querySelector('.small')
 let betAmount = document.querySelector('#betAmount')
-const betBtn = document.querySelector('.betBtn')
 let gameMessage = document.querySelector('.gameMessage')
 let moneyDisplay = document.querySelector('.moneyDisplay')
-let randomThreeOfAKind = document.querySelector('.randomToak')
 let gameChoice = ''
 let bank = 500
 let diceArr = []
@@ -74,7 +69,6 @@ const compare = () => {
       loseMoney()
     }
   } else if (gameChoice === 'threeOfAKind') {
-    console.log(diceArr)
     if (diceArr[0] === diceArr[1] && diceArr[1] === diceArr[2]) {
       if (tok === diceArr[0]) {
         gameMessage.innerText = 'Win'
@@ -103,7 +97,6 @@ const placeBet = (event) => {
     gameMessage.innerText = 'Not enough money in the bank'
   } else {
     diceArr = []
-    // diceArr = [5, 5, 5]
     rollingDice()
     diceResult.innerText = ''
     diceResult.innerText = diceTotal()
@@ -146,8 +139,6 @@ const betRandom = () => {
 const assignTokOne = () => {
   gameChoice = 'threeOfAKind'
   tok = 1
-  console.log(diceArr)
-  console.log(tok)
 }
 
 const assignTokTwo = () => {
@@ -175,10 +166,10 @@ const assignTokSix = () => {
   tok = 6
 }
 
-betBtn.addEventListener('click', placeBet)
-big.addEventListener('click', placeBig)
-small.addEventListener('click', placeSmall)
-randomThreeOfAKind.addEventListener('click', betRandom)
+document.querySelector('.betBtn').addEventListener('click', placeBet)
+document.querySelector('.big'), addEventListener('click', placeBig)
+document.querySelector('.small').addEventListener('click', placeSmall)
+document.querySelector('.randomToak').addEventListener('click', betRandom)
 document.querySelector('.threeOne').addEventListener('click', assignTokOne)
 document.querySelector('.threeTwo').addEventListener('click', assignTokTwo)
 document.querySelector('.threeThree').addEventListener('click', assignTokThree)
